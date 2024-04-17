0f7fbe30066faff9530e0e5ad9aba6acaab27da2
DaviRdaSilva <davi.rsilva@sptech.school>
2024-04-17 17:44:45 -0300

    style-Reciação e junção dos codigos feitos

---
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/ProjetoTfgApplication.kt
@@ -0,0 +1,11 @@
+package school.sptech.projetotfg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoTfgApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoTfgApplication>(*args)
+}
