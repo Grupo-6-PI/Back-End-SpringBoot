08d017da482f0e0643d41967e7f14acf1562e215
DaviRdaSilva <davi.rsilva@sptech.school>
2024-04-17 17:44:45 -0300

    style-Reciação e junção dos codigos feitos

--- Login_LogOff/Login_LogOff/src/main/kotlin/BackEndTFG/Login_LogOff/Repository/AuthRepository.kt
+++
@@ -1,8 +0,0 @@
-package BackEndTFG.Login_LogOff.Repository
-
-import BackEndTFG.Login_LogOff.Dominio.AuthUser
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AuthRepository : JpaRepository<AuthUser, Long> {
-    fun findByEmail(email: String): AuthUser?
-}
