3600abf2e8d8f1e83a3393261941c1a1f9fcbfc0
Rasmussen <bruna.rasmussen@accenture.com>
2024-04-19 19:47:17 -0300

    ALT domain usuario validacao

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/domain/Usuario.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/domain/Usuario.kt
@@ -1,14 +1,25 @@
-package com.example.v1backtfg
+package com.example.projeto
 
+import java.time.LocalDate
+
+-tfg
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.Size
 import java.time.LocalDateTime
 import java.util.Date
 
-class Usuario (
-    val id:Int,
-    val nome:String,
-    val sobrenome:String,
-    val dtNascimento:String,
-    val nivelAcesso:Int
+@Entity
+data class Usuario(
+    @field:Id
+    @field:GeneratedValue(strategy = GenerationType.IDENTITY) var id: Int?,
+    @field:NotBlank  var nome:String,
+    @field:NotBlank var sobrenome:String,
+    @field:NotNull @field:Past var dtNascimento:LocalDate,
+    @field:NotBlank val nivelAcesso:Int
 ) {
 
     fun verificacaoCadastro(usuario:Usuario):Boolean{
