9d3e2711c555e57012a8938c461e00eb138fddf2
Rasmussen <bruna.rasmussen@accenture.com>
2024-04-20 11:45:54 -0300

    ALT-User-melhoramento de codigo

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/domain/Cidade.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/domain/Cidade.kt
@@ -2,10 +2,12 @@ package school.sptech.projetotfg.domain
 
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.Max
 
 class Cidade(
     @field:Id @field:GeneratedValue(strategy = GenerationType.IDENTITY)
     var idCidade:Int,
-    @field:Maxvar nome:String
+    @field:Max(30) var nome:String
 ) {
 }
