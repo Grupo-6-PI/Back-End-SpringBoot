995171d3d865e13d4f14bbfb0eabbc85c7aa61dc
Zuull21 <julio.dahi@sptech.school>
2024-05-07 21:33:08 -0300

    fix - correção de bugs de cadastro de usuario e de login, é necessário verificar logout

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/AcessoController.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/AcessoController.kt
@@ -2,21 +2,27 @@ package school.sptech.projetotfg.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import school.sptech.projetotfg.domain.Usuario
+import school.sptech.projetotfg.dto.cadastrarUsuarioResponse
 import school.sptech.projetotfg.repository.AcessoRepository
 import school.sptech.projetotfg.repository.UsuarioRepository
+import org.modelmapper.ModelMapper
+import school.sptech.projetotfg.domain.Acesso
+import school.sptech.projetotfg.dto.LoginAcessoResponse
 
 @RestController
 @RequestMapping("/acesso")
-class AcessoController(private val AcessoRepository: AcessoRepository, private val UsuarioRepository: UsuarioRepository) {
+class AcessoController(
+    private val AcessoRepository: AcessoRepository,
+    private val UsuarioRepository: UsuarioRepository,
+    val mapper: ModelMapper= ModelMapper()
+) {
 
     @PostMapping("/login")
-    fun login(@RequestParam email: String, @RequestParam password: String): String {
-        val user = UsuarioRepository.findByEmail(email)
-        return if (user != null && user.senha == password) {
-            "Login feito com sucesso"
-        } else {
-            "Nome ou senha invalidos"
-        }
+    fun login(@RequestBody usuario: LoginAcessoResponse): ResponseEntity<String> {
+        var usuarioLogado: Usuario = mapper.map(usuario,Usuario::class.java)
+
+         return ResponseEntity.status(201).body("Login feito")
     }
 
     @PostMapping("/logout")
