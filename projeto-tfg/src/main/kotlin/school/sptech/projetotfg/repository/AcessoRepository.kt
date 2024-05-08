5739bd8bd1dfe77b26dc5b2126d1d41d18fa5c29
PauloCafasso <paulo.cafasso@sptech.school>
2024-05-07 22:26:10 -0300

    fix/variável global para permitir logout

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/repository/AcessoRepository.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/repository/AcessoRepository.kt
@@ -7,6 +7,7 @@ import school.sptech.projetotfg.domain.Acesso
 interface AcessoRepository : JpaRepository<Acesso, Int> {
     @Query("SELECT situacao FROM Acesso WHERE id = 1")
     fun logout(id: Int): String {
+
             return("Logout executado!")
     }
 }
