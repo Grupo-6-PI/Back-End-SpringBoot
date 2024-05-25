fbf600b285c53eb45876a352e0304f3d8714ae32
Zuull21 <julio.dahi@sptech.school>
2024-05-25 16:51:19 -0300

    featch - criação de DTOS, Repositorys e Services para cadastro

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/UsuarioController.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/UsuarioController.kt
@@ -1,65 +1,26 @@
 package school.sptech.projetotfg.controller
 
-import org.modelmapper.ModelMapper
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import school.sptech.projetotfg.domain.cadastro.Usuario
-import school.sptech.projetotfg.repository.UsuarioRepository
-import school.sptech.projetotfg.dto.CadastrarUsuarioResponse;
-
+import school.sptech.projetotfg.dto.BeneficiarioInputDTO
+import school.sptech.projetotfg.dto.BeneficiarioResponseDTO
+import school.sptech.projetotfg.service.BeneficiarioService
 
 @RestController
-@RequestMapping("/usuario")
-class UsuarioController(private val repository: UsuarioRepository, val mapper: ModelMapper= ModelMapper()) {
-
-
-    @GetMapping("/listar_usuarios")
-    fun getListaUsuariosCadastrados():ResponseEntity<List<Usuario>>{
-        val listaUsuariosCadastrados = repository.findAll()
-        if(listaUsuariosCadastrados.isNotEmpty()){
-            return ResponseEntity.status(200).body(listaUsuariosCadastrados)
-        }
-            return ResponseEntity.status(204).build()
-    }
-
-    @GetMapping("/buscar_usuario/{id}")
-    fun getUsuarioPorId(@PathVariable id:Int):ResponseEntity<Usuario>{
-        if(repository.existsById(id)){
-            val usuarioEncontrado = repository.findById(id).get()
-            return ResponseEntity.status(200).body(usuarioEncontrado)
-        }
-            return ResponseEntity.status(404).build()
-    }
+@RequestMapping("/beneficiarios")
+class UsuarioController(
+    private val beneficiarioService: BeneficiarioService
+) {
 
     @PostMapping
-    fun postUsuario(@RequestBody usuario:CadastrarUsuarioResponse):ResponseEntity<Usuario>{
-
-        var usuarioNovo: Usuario = mapper.map(usuario, Usuario::class.java)
-
-        return ResponseEntity.status(201).body(usuarioNovo)
-    }
-
-    @PutMapping("/alterar_dados")
-    fun putUsuarioPorId(@RequestBody dadosNovosDoUsuario: Usuario):ResponseEntity<Usuario>{
-            if (repository.existsById(dadosNovosDoUsuario.idUsuario)){
-                val usuario: Usuario = repository.findById(dadosNovosDoUsuario.idUsuario).get()
-                usuario.setNome(dadosNovosDoUsuario.getNome())
-                usuario.email = dadosNovosDoUsuario.email
-                usuario.informacoesAdicionais = dadosNovosDoUsuario.informacoesAdicionais
-                usuario.situacao = dadosNovosDoUsuario.situacao
-                repository.save(usuario)
-                return ResponseEntity.status(200).body(usuario)
-            }
-                return ResponseEntity.status(404).build()
-    }
-
-    @DeleteMapping("/{id}")
-    fun deleteUsuarioPorId(@PathVariable id:Int):ResponseEntity<String>{
-        if(repository.existsById(id)){
-            repository.deleteById(id)
-            return ResponseEntity.status(200).body("Usuario removido com sucesso!")
+    fun cadastrarBeneficiario(@Validated @RequestBody dto: BeneficiarioInputDTO): ResponseEntity<BeneficiarioResponseDTO> {
+        return try {
+            val responseDTO = beneficiarioService.cadastrarBeneficiario(dto)
+            ResponseEntity(responseDTO, HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null)
         }
-            return ResponseEntity.status(404).build()
     }
-
 }
---
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/dto/BeneficiarioResponseDTO.kt
@@ -0,0 +1,7 @@
+package school.sptech.projetotfg.dto
+
+data class BeneficiarioResponseDTO(
+    val idUsuario: Int,
+    val nome: String,
+    val email: String
+)
