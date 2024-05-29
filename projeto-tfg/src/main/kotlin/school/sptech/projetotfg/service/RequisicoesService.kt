8ab7c962616ce2dcce9b447ad4aad60e1aaaac28
DaviRdaSilva <davi.rsilva@sptech.school>
2024-05-29 16:49:38 -0300

    fix - Erros de inicialização do BackEnd

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/service/RequisicoesService.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/service/RequisicoesService.kt
@@ -2,10 +2,12 @@ package school.sptech.projetotfg.service
 
 import org.modelmapper.ModelMapper
 import org.springframework.http.HttpStatusCode
+import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import school.sptech.projetotfg.dto.RequisicoesDoacaoResponseDTO
 import school.sptech.projetotfg.repository.RequisicaoRepository
 
+@Service
 class RequisicoesService (
     val requisicaoRepository: RequisicaoRepository,
     val mapper: ModelMapper
