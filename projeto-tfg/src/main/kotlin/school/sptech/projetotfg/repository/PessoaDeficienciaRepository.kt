61994f2931e37c42bb0c37f9c242d1c7c8c061b5
Zuull21 <julio.dahi@sptech.school>
2024-06-02 14:24:20 -0300

    refactor - Refatorração do Login e Logoff do usuario de acordo com nova modelagem e classes

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/repository/PessoaDeficienciaRepository.kt
+++
@@ -1,6 +0,0 @@
-package school.sptech.projetotfg.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PessoaDeficienciaRepository : JpaRepository<PessoaDeficiencia, Int> {
-}
