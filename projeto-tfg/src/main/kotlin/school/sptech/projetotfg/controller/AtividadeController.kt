fedd3a13840af5883e9a8f8cfa562e4c248d6dda
PauloCafasso <paulo.cafasso@sptech.school>
2024-06-10 20:07:30 -0300

    feat- end-point de consulta de atividade por id

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/AtividadeController.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/controller/AtividadeController.kt
@@ -1,13 +1,11 @@
 package school.sptech.projetotfg.controller
 
-import org.modelmapper.ModelMapper
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import school.sptech.projetotfg.domain.atividades.Atividade
 import school.sptech.projetotfg.dto.AtividadeResponseDTO
-import school.sptech.projetotfg.repository.AtividadeRepository
 import school.sptech.projetotfg.service.AtividadeService
 
 @RestController
@@ -19,6 +17,12 @@ class AtividadeController (
     fun listarAtividades():ResponseEntity<List<AtividadeResponseDTO>>{
         val atividades = atividadeService.listarAtivadade()
         return ResponseEntity.status(200).body(atividades)
+    }
+
+    @GetMapping("/id")
+    fun atividadePorId(@RequestParam id:Int): ResponseEntity<AtividadeResponseDTO> {
+        val atividadeEncontrada = atividadeService.atividadePorId(id)
+        return ResponseEntity.status(200).body(atividadeEncontrada)
     }
 
 }
