6b576c034081d8f9e194d56d194d85c485b2b9c6
PauloCafasso <paulo.cafasso@sptech.school>
2024-06-10 20:07:30 -0300

    feat- end-point de consulta de atividade por id

--- projeto-tfg/src/main/kotlin/school/sptech/projetotfg/service/AtividadeService.kt
+++ projeto-tfg/src/main/kotlin/school/sptech/projetotfg/service/AtividadeService.kt
@@ -1,11 +1,9 @@
 package school.sptech.projetotfg.service
 
 import org.modelmapper.ModelMapper
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
-import school.sptech.projetotfg.domain.atividades.Atividade
 import school.sptech.projetotfg.dto.AtividadeResponseDTO
 import school.sptech.projetotfg.repository.AtividadeRepository
 
@@ -26,4 +24,14 @@ class AtividadeService(
             }
             throw ResponseStatusException(HttpStatusCode.valueOf(204))
     }
+
+    fun atividadePorId(id:Int):AtividadeResponseDTO{
+        val atividade = atividadeRepository.findById(id).get()
+        if(!atividade.equals(null)){
+            val atividadeDto:AtividadeResponseDTO = mapper.map(atividade, AtividadeResponseDTO::class.java)
+            return atividadeDto
+        }
+        throw ResponseStatusException(HttpStatusCode.valueOf(404))
+    }
+
 }
