2e598912c775ab62891e2740aa9b5a9b19cb03ed
DaviRdaSilva <davi.rsilva@sptech.school>
2024-08-26 14:13:11 -0300

    Refactor/Adição de Get/Set no quadrante Atividade

--- src/main/kotlin/school/sptech/projetotfg/domain/atividades/Repeticao.kt
+++ src/main/kotlin/school/sptech/projetotfg/domain/atividades/Repeticao.kt
@@ -9,8 +9,41 @@ import jakarta.validation.constraints.Positive
 class Repeticao (
     @field:Id
     @field:GeneratedValue(strategy = GenerationType.IDENTITY) private var id: Long?,
-    @field:Positive private var quantidade:Long?,
+    @field:Positive private var quantidade:Int?,
     @field:NotBlank @field:Max(45) private var escolhaRepeticao:String?,
-    @field:ManyToOne private var disponibilidadeVoluntario:DisponibilidadeUsuario?
+    @field:ManyToOne private var disponibilidadeVoluntario:DisponibilidadeVoluntario?
 ){
+
+    fun getId():Long{
+        return id!!
+    }
+
+    fun setId(new: Long){
+        this.id = new
+    }
+
+    fun getQuantidade():Int{
+        return quantidade!!
+    }
+
+    fun setQuantidade(new:Int){
+        this.quantidade = new
+    }
+
+    fun getEscolhaRepeticao():String{
+        return escolhaRepeticao!!
+    }
+
+    fun setEscolhaRepeticao(new:String){
+        this.escolhaRepeticao = new
+    }
+
+    fun getDisponibilidadeVoluntario():DisponibilidadeVoluntario{
+        return disponibilidadeVoluntario!!
+    }
+
+    fun setDisponibilidadeVoluntario(new:DisponibilidadeVoluntario){
+        this.disponibilidadeVoluntario = new
+    }
+
 }
