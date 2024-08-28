f00babe61fb262a193b58a6d5a64028f812cc04a
DaviRdaSilva <davi.rsilva@sptech.school>
2024-08-28 18:57:41 -0300

    Refactor/Endpoints de Atividade

--- src/main/kotlin/school/sptech/projetotfg/dto/AtividadeResponseDTO.kt
+++ src/main/kotlin/school/sptech/projetotfg/dto/AtividadeResponseDTO.kt
@@ -1,11 +1,12 @@
 package school.sptech.projetotfg.dto
 
 import java.time.LocalDate
+import java.time.LocalTime
 
 data class AtividadeResponseDTO(
-    val id:Long,
-    val nome:String,
-    val tipo:String,
-    val comeco:LocalDate,
-    val descricao:String
+    val id:Long?,
+    val nome:String?,
+    val tipo:String?,
+    val comeco:LocalTime?,
+    val descricao:String?
 )
