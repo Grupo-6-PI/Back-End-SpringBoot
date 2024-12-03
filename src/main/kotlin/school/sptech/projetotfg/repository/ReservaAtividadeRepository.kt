052f2bcecc8604c8206277de980b4be32e2e67d8
DaviRdaSilva <davi.rsilva@sptech.school>
2024-12-03 14:26:49 -0300

    COrreção de DTO data

--- src/main/kotlin/school/sptech/projetotfg/repository/ReservaAtividadeRepository.kt
+++ src/main/kotlin/school/sptech/projetotfg/repository/ReservaAtividadeRepository.kt
@@ -3,11 +3,13 @@ package school.sptech.projetotfg.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import school.sptech.projetotfg.domain.atividades.Calendario
 import school.sptech.projetotfg.domain.atividades.ReservaAtividade
+import java.util.Optional
 
 interface ReservaAtividadeRepository : JpaRepository<ReservaAtividade, Long> {
     fun findAllByCalendarioId(calendario: Long): List<ReservaAtividade>
 
+    fun existsByAtividadeId(id:Long): Boolean
 
-    fun findByAtividadeId(id:Long):ReservaAtividade?
+    fun findByAtividadeId(id:Long):Optional<ReservaAtividade>
 
 }
